714f6a11482d355f89289d73a9688efeed568eef
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-09-21 17:59:55 +0700

    Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: Heading(
-            text: 'Hello World',
-          ),
+          child: BiggerText(text: "multiplatform go brrrr"),
         ),
       ),
     );
@@ -37,6 +35,37 @@ class Heading extends StatelessWidget {
         fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text('perbesar'),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
