43e5446692be3ac315a4c9c3e74b1e83e3e684f0
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-09-22 14:32:36 +0700

    padding and center

--- lib/main.dart
+++ lib/main.dart
@@ -11,65 +11,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FirstScreen(),
-    );
-  }
-}
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: const Center(
+        //   child: Text(
+        //     'Text berada di tengah',
+        //     style: TextStyle(
+        //         color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 40),
+        //   ),
+        // ),
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            onPressed: () {},
+        home: const Padding(
+          padding: EdgeInsets.all(30),
+          child: Text(
+            'ini padding',
+            style: TextStyle(
+                color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 40),
           ),
-        ],
-        leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          onPressed: () {},
-        ),
-      ),
-
-      //container is inside the body
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.red,
-          border: Border.all(color: Colors.green, width: 3),
-          borderRadius: BorderRadius.circular(10),
-          // shape: BoxShape.circle,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black,
-              offset: Offset(3, 6),
-              blurRadius: 10,
-            )
-          ],
-        ),
-        child: const Text(
-          'Hi',
-          style: TextStyle(fontSize: 40),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
-      ),
-    );
+        ));
   }
 }
