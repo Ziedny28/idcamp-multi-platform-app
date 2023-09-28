1c22f544ddaf65d339832b37782d0b1acbc849b9
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-09-28 19:09:46 +0700

    switch

--- lib/main.dart
+++ lib/main.dart
@@ -10,110 +10,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const InputAndPopup(),
+      home: const SwitchExample(),
     );
   }
 }
 
-class InputAndPopup extends StatefulWidget {
-  const InputAndPopup({super.key});
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
 
   @override
-  State<InputAndPopup> createState() => _InputAndPopup();
+  State<SwitchExample> createState() => _SwitchExample();
 }
 
-class _InputAndPopup extends State<InputAndPopup> {
-  String _name = '';
+class _SwitchExample extends State<SwitchExample> {
+  bool lightOn = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('First Screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                hintText: 'Write your name here',
-                labelText: 'Your Name',
+      body: Switch(
+          value: lightOn,
+          onChanged: (bool value) {
+            setState(() {
+              lightOn = value;
+            });
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(lightOn ? 'LightOn' : 'LightOff'),
+                duration: const Duration(seconds: 1),
               ),
-              onChanged: (String value) {
-                setState(() {
-                  _name = value;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      content: Text('Helo $_name'),
-                    );
-                  },
-                );
-              },
-              child: const Text('submit'),
-            )
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
-
-// class InputWidget extends StatefulWidget {
-//   const InputWidget({super.key});
-
-//   @override
-//   State<InputWidget> createState() => _InputWidget();
-// }
-
-// class _InputWidget extends State<InputWidget> {
-//  final String _name = '';
-//  final TextEditingController _controller = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//    // taking number when there's change
-//    return Scaffold(
-//      body: TextField(
-//        onChanged: (String value) {
-//          setState(() {
-//            _name = value;
-//          });
-//        },
-//      ),
-//    );
-
-//   // taking input after submit
-//    return Scaffold(
-//      body: TextField(
-//        onSubmitted: (String value) {
-//          setState(() {
-//            _name = value;
-//          });
-//        },
-//      ),
-//    );
-
-//   // using TextEditingController
-
-//    return Scaffold(
-//      body: TextField(
-//        controller: _controller,
-//      ),
-//    );
-//   }
-
-// // for more efficient memory usage when using TextEditingController use this
-//  @override
-//  void dispose() {
-//    _controller.dispose();
-//    super.dispose();
-//  }
-// }
