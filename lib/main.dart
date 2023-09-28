edc8860ce8d95d2ae4273e021d34b4757eba204a
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-09-28 18:49:35 +0700

    button and popup

--- lib/main.dart
+++ lib/main.dart
@@ -10,59 +10,110 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const InputWidget(),
+      home: const InputAndPopup(),
     );
   }
 }
 
-class InputWidget extends StatefulWidget {
-  const InputWidget({super.key});
+class InputAndPopup extends StatefulWidget {
+  const InputAndPopup({super.key});
 
   @override
-  State<InputWidget> createState() => _InputWidget();
+  State<InputAndPopup> createState() => _InputAndPopup();
 }
 
-class _InputWidget extends State<InputWidget> {
-  // final String _name = '';
-  final TextEditingController _controller = TextEditingController();
-
+class _InputAndPopup extends State<InputAndPopup> {
+  String _name = '';
   @override
   Widget build(BuildContext context) {
-    // taking number when there's change
-    // return Scaffold(
-    //   body: TextField(
-    //     onChanged: (String value) {
-    //       setState(() {
-    //         _name = value;
-    //       });
-    //     },
-    //   ),
-    // );
-
-    //taking input after submit
-    // return Scaffold(
-    //   body: TextField(
-    //     onSubmitted: (String value) {
-    //       setState(() {
-    //         _name = value;
-    //       });
-    //     },
-    //   ),
-    // );
-
-    //using TextEditingController
-
-    // return Scaffold(
-    //   body: TextField(
-    //     controller: _controller,
-    //   ),
-    // );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Write your name here',
+                labelText: 'Your Name',
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  _name = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text('Helo $_name'),
+                    );
+                  },
+                );
+              },
+              child: const Text('submit'),
+            )
+          ],
+        ),
+      ),
+    );
   }
-
-  //for more efficient memory usage when using TextEditingController use this
-  // @override
-  // void dispose() {
-  //   _controller.dispose();
-  //   super.dispose();
-  // }
 }
+
+// class InputWidget extends StatefulWidget {
+//   const InputWidget({super.key});
+
+//   @override
+//   State<InputWidget> createState() => _InputWidget();
+// }
+
+// class _InputWidget extends State<InputWidget> {
+//  final String _name = '';
+//  final TextEditingController _controller = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//    // taking number when there's change
+//    return Scaffold(
+//      body: TextField(
+//        onChanged: (String value) {
+//          setState(() {
+//            _name = value;
+//          });
+//        },
+//      ),
+//    );
+
+//   // taking input after submit
+//    return Scaffold(
+//      body: TextField(
+//        onSubmitted: (String value) {
+//          setState(() {
+//            _name = value;
+//          });
+//        },
+//      ),
+//    );
+
+//   // using TextEditingController
+
+//    return Scaffold(
+//      body: TextField(
+//        controller: _controller,
+//      ),
+//    );
+//   }
+
+// // for more efficient memory usage when using TextEditingController use this
+//  @override
+//  void dispose() {
+//    _controller.dispose();
+//    super.dispose();
+//  }
+// }
