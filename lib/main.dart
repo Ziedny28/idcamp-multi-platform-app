140bce142b08c3b6e8e55097d6f16352cd831c59
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-09-29 05:18:48 +0700

    wisata ngawi

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wisata Bandung',
+      title: 'Wisata Ngawi',
       theme: ThemeData(),
       home: const ImageAssetExample(),
     );
